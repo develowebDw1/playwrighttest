1c7e1a8392f1723fa54607fc0577132a9a08116a
develowebDw1 <develoweb.dw1@gmail.com>
2022-06-14 11:09:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { chromium } = require('playwright')
+
+const app = express();
+
+app.get("/", async function(req, res) {
+
+    const browser = await chromium.launch()
+
+    const page = await browser.newPage()
+    
+    await page.goto('https://www.cineplanet.com.pe/cinemas/cp-alcazar')
+
+    const element = await page.waitForSelector('.cinemas-details--header');
+
+    await page.screenshot({ path: 'xbox.png' })
+
+    await browser.close()
+    
+    res.send("Bienvenido a la api de crawlers");
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log('listening on port ' + PORT)
+});
